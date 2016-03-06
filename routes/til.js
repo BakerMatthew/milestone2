d52accc63d68d7b7ddd299ba6bb70bc8c6716f17
BakerMatthew <matt@bakercrew.com>
2016-03-05 18:19:53 -0700

    Attempt to make the website more attractive

--- routes/til.js
+++ routes/til.js
@@ -1,34 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+//Stores users til entries
 var entries = [
-  {info:"My First Entry", body: "Hello World! This is my first entry!", created_at: "some date"},
-  {info:"Learning JS", body: "Sometimes linking pages together is difficult...", created_at: "some date"}
+  {info:"My First Entry", body: "Hello World! This is my first entry!", created_at: "March 1, 2016"},
+  {info:"Learning JS", body: "Sometimes linking pages together is difficult... But I got it down!", created_at: "March 4, 2016"}
 ];
 
 /* READ all: GET til listing. */
+//Sets up initial page
 router.get('/', function(req, res, next) {
   res.render('til/index', { title: 'Today I Learned', entries: entries });
 });
 
-//ATTEMPT:
-/* /til/* */
+
+/* CREATE entry: /til/# */
+//Shows a single entry
 router.get('/:id', function(req, res, next) {
   res.render('til/entry', { title: 'View An Entry', entry: entries[req.params.id]});
 });
 
 /* CREATE new entry form: GET /til/new */
+//Sets up new entry page
 router.get('/new', function(req, res, next) {
   res.render('til/new', {title: "Create A New entry"});
 });
 
 /*CREATE new entry: POST /til/ */
+//Shows the new entry page
 router.post('/', function(req, res, next) {
   entries.push(req.body);
   res.render('til/index', { title: 'Today I Learned', entries: entries });
 });
 
 /* UPDATE edit entry form: GET /til/1/edit */
+//Sets up edit page
 router.get('/:id/edit', function(req, res, next) {
   res.render('til/update',
   {
@@ -39,6 +45,7 @@ router.get('/:id/edit', function(req, res, next) {
 });
 
 /* UPDATE edit entry: POST /til/1 */
+//Shows the edit entry page
 router.post('/:id', function(req, res, next) {
   entries[req.params.id] = req.body;
   res.render('til/index',
@@ -49,6 +56,7 @@ router.post('/:id', function(req, res, next) {
 });
 
 /* DELETE delete entry: GET /til/1/delete  */
+//Deletes an entry
 router.get('/:id/delete', function(req, res, next) {
   var id = req.params.id
   entries = entries.slice(0,id).concat(entries.slice(id+1, entries.length));
