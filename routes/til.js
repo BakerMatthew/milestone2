2bf48da06f004062fc42821c6f58065ef9f8c95e
BakerMatthew <matt@bakercrew.com>
2016-03-05 18:14:10 -0700

    Attempt to fix entry route

--- routes/til.js
+++ routes/til.js
@@ -11,18 +11,24 @@ router.get('/', function(req, res, next) {
   res.render('til/index', { title: 'Today I Learned', entries: entries });
 });
 
-/* CREATE entry form: GET /til/new */
+//ATTEMPT:
+/* /til/* */
+router.get('/:id', function(req, res, next) {
+  res.render('til/entry', { title: 'View An Entry', entry: entries[req.params.id]});
+});
+
+/* CREATE new entry form: GET /til/new */
 router.get('/new', function(req, res, next) {
   res.render('til/new', {title: "Create A New entry"});
 });
 
-/*CREATE entry: POST /til/ */
+/*CREATE new entry: POST /til/ */
 router.post('/', function(req, res, next) {
   entries.push(req.body);
   res.render('til/index', { title: 'Today I Learned', entries: entries });
 });
 
-/* UPDATE entry form: GET /til/1/edit */
+/* UPDATE edit entry form: GET /til/1/edit */
 router.get('/:id/edit', function(req, res, next) {
   res.render('til/update',
   {
@@ -32,7 +38,7 @@ router.get('/:id/edit', function(req, res, next) {
   });
 });
 
-/* UPDATE entry: POST /til/1 */
+/* UPDATE edit entry: POST /til/1 */
 router.post('/:id', function(req, res, next) {
   entries[req.params.id] = req.body;
   res.render('til/index',
@@ -42,27 +48,7 @@ router.post('/:id', function(req, res, next) {
   });
 });
 
-/* UPDATE entry form: GET /til/1/entry */
-router.get('/:id/entry', function(req, res, next) {
-  res.render('til/entry',
-  {
-    title: 'View an entry',
-    id: req.params.id,
-    entry: entries[req.params.id]
-  });
-});
-
-/* UPDATE entry: POST /til/1 */
-router.post('/:id', function(req, res, next) {
-  entries[req.params.id] = req.body;
-  res.render('til/index',
-  {
-    title: 'View an entry',
-    entries: entries
-  });
-});
-
-/* DELETE entry: GET /til/1/delete  */
+/* DELETE delete entry: GET /til/1/delete  */
 router.get('/:id/delete', function(req, res, next) {
   var id = req.params.id
   entries = entries.slice(0,id).concat(entries.slice(id+1, entries.length));
