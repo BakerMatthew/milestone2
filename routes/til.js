0b68afd79ad1248c84150adde1788889adc6e0c8
BakerMatthew <matt@bakercrew.com>
2016-03-26 19:11:45 -0600

    Added database

--- routes/til.js
+++ routes/til.js
@@ -2,15 +2,37 @@ var express = require('express');
 var router = express.Router();
 
 //Stores users til entries
-var entries = [
-  {info:"My First Entry", body: "Hello World! This is my first entry!", created_at: "March 1, 2016"},
-  {info:"Learning JS", body: "Sometimes linking pages together is difficult... But I got it down!", created_at: "March 4, 2016"}
-];
+var entries = [];
 
 /* READ all: GET til listing. */
 //Sets up initial page
 router.get('/', function(req, res, next) {
-  res.render('til/index', { title: 'Today I Learned', entries: entries });
+  req.db.driver.execQuery(
+    "SELECT * FROM entries;",
+    function(err, data) {
+      if(err)
+      {
+        console.log(err);
+      }
+      res.render('til/index', { title: 'Today I Learned', entries: data });
+    }
+  );
+});
+
+/*CREATE new entry: POST /til/ */
+//Shows the new entry page
+router.post('/', function(req, res, next) {
+  req.db.driver.execQuery(
+    "INSERT INTO entries (info,body) VALUES (?,?);",
+    [req.body.info, req.body.body],
+    function(err, data){
+      if(err)
+      {
+        console.log(err);
+      }
+      res.redirect(303, '/til/index');
+    }
+  );
 });
 
 /* CREATE new entry form: GET /til/new */
@@ -19,47 +41,74 @@ router.get('/new', function(req, res, next) {
   res.render('til/new', {title: "Create A New entry"});
 });
 
-/*CREATE new entry: POST /til/ */
-//Shows the new entry page
-router.post('/', function(req, res, next) {
-  entries.push(req.body);
-  res.render('til/index', { title: 'Today I Learned', entries: entries });
-});
-
 /* UPDATE edit entry form: GET /til/1/edit */
 //Sets up edit page
 router.get('/:id/edit', function(req, res, next) {
-  res.render('til/update',
-  {
-    title: 'Update an entry',
-    id: req.params.id,
-    entry: entries[req.params.id]
-  });
+  req.db.driver.execQuery(
+    'SELECT * FROM entries WHERE id=?;',
+    [paseIt(eq.params.id)],
+    function(err, data) {
+      if(err)
+      {
+        console.log(err);
+      }
+      res.render('til/update',
+      {
+        title: 'Update an entry',
+        entry: data[0]
+      });
+    }
+  );
 });
 
 /* UPDATE edit entry: POST /til/1 */
 //Shows the edit entry page
 router.post('/:id', function(req, res, next) {
-  entries[req.params.id] = req.body;
-  res.render('til/index',
-  {
-    title: 'Update an entry',
-    entries: entries
-  });
+  var id=parseInt(req.params.id);
+  
+  req.db.driver.execQuery(
+    "UPDATE entries SET info=?, body=? WHERE id=?;",
+    [req.body.info, req.body.body, parseInt(req.params.id)],
+    function(err, data) {
+      if(err)
+      {
+        console.log(err);
+      }
+      res.redirect(303, '/til/' + id),
+    }
+  );
 });
 
 /* DELETE delete entry: GET /til/1/delete  */
 //Deletes an entry
 router.get('/:id/delete', function(req, res, next) {
-  var id = req.params.id
-  entries = entries.slice(0,id).concat(entries.slice(id+1, entries.length));
-  res.render('til/index', { title: 'Today I Learned', entries: entries });
+  req.db.driver.execQuery(
+    'DELETE FROM entries WHERE id=?;',
+    [parseInt(req.params.id)],
+    function(err, data) {
+      if(err)
+      {
+        console.log(err);
+      }
+      res.redirect(303, '/til/');
+    }
+  );
 });
 
 /* GET til listing. */
 //Show single entry page
 router.get('/:id', function(req, res, next) {
-  res.render('til/entry', { title: 'An entry' , entry: entries[req.params.id]});
+  req.db.driver.execQuery(
+    'SELECT * FROM entries WHERE id=?;',
+    [parseInt(req.params.id)],
+    function(err, data) {
+      if(err)
+      {
+        console.log(err);
+      }
+      res.render('til/entry', { title: 'An entry' , entry: data[0]});
+    }
+  );
 });
 
 module.exports = router;
